4011bb4b34b26a74df2b4c2124cda89a26d19e73
slinkydeveloper <francescoguard@gmail.com>
2019-04-20 14:48:33 +0200

    Configured doc gen, swaggerui and spec serve Wrote first operation handler

--- index.js
+++ index.js
@@ -1,37 +1,46 @@
-// const fs = require("fs");
-// const express = require("express");
-// const { OpenApiValidator } = require("express-openapi-validate/dist/index");
-// const jsYaml = require("js-yaml");
-//
-// const app = express();
-// app.use(express.json());
-//
-// const openApiDocument = jsYaml.safeLoad(
-//     fs.readFileSync("openapi.yaml", "utf-8")
-// );
-// const validator = new OpenApiValidator(openApiDocument);
-//
-// app.get("/user", validator.validate("get", "/user"), (req, res, next) => {
-//     res.json({ hello: "world" });
-// });
-//
-// app.use((err, req, res, next) => {
-//     const statusCode = err.statusCode || 500;
-//     res.status(statusCode).json({
-//         error: {
-//             name: err.name,
-//             message: err.message,
-//             data: err.data,
-//         },
-//     });
-// });
-//
-// const server = app.listen(3000, () => {
-//     console.log("Listening on", server.address());
-// });
-
+const fs = require("fs");
+const express = require("express");
+const { OpenApiValidator } = require("express-openapi-validate/dist/index");
+const jsYaml = require("js-yaml");
 const Sequelize = require('sequelize');
-const env = process.env.NODE_ENV || 'development';
-const config = require('./config/config.json')[env];
+const config = require('./config/config.json')[process.env.NODE_ENV || 'development'];
 const sequelize = new Sequelize(config.database, config.username, config.password, config);
-const model = require('./models/index.js').init(sequelize);
+require('./models/index.js').init(sequelize);
+const pathToSwaggerUi = require('swagger-ui-dist').absolutePath()
+const cors = require('cors')
+
+//TODO reorder shit
+
+const openApiDocument = jsYaml.safeLoad(
+    fs.readFileSync("openapi.yaml", "utf-8")
+);
+const validator = new OpenApiValidator(openApiDocument);
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Mount Spec
+app.get('/backend/spec.yaml', (req, res) => res.sendFile(__dirname + '/openapi.yaml'));
+// Mount backend docs and stuff
+app.get('/backend/main.html', (req, res) => res.redirect('/backend'))
+app.use('/backend', express.static('backend_public'));
+
+// Mount api paths
+require('./paths/books')(app, validator);
+
+// Fallback error handler
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    res.status(statusCode).json({
+        error: {
+            name: err.name,
+            message: err.message,
+            data: err.data,
+        },
+    });
+});
+
+const server = app.listen(process.env.PORT || 3000, () => {
+    console.log("Listening on", server.address());
+});
--- paths/books.js
+++ paths/books.js
@@ -1,3 +1,32 @@
-module.exports = app => {
+const model = require('../models/index.js')
+const _ = require('lodash')
 
-}
+module.exports = (app, validator) => {
+
+    app.get('/book/:bookId', validator.validate('get', '/book/{bookId}'), (req, res, next) => {
+        let bookId = req.params.bookId;
+        model.Book.findByPk(bookId, {
+            include: [
+                {
+                    model: model.Genre,
+                    as: 'Genre'
+                },
+                {
+                    model: model.Theme,
+                    as: 'Theme'
+                },
+                {
+                    model: model.Author,
+                    as: 'Author'
+                }
+            ]
+        }).then(
+            book =>
+                _.isNull(book) || _.isUndefined(book) ?
+                    res.status(404).end() :
+                    res.json(_.pick(book.toJSON(), ['isbn', 'name', 'Author', 'Theme', 'Genre', 'BookEvent'])),
+            res.next
+        );
+    })
+
+};
